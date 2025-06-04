581739dbf5a70bfc3db1b03ee58359549762a562
Monire En <monire.eshghi@gmail.com>
2025-06-04 00:24:09 -0400

    Add initial application structure and styling for Movie.ca
    - Created App component with welcome message and featured movies list.
    - Added App.css for styling the application layout and logo animations.
    - Included index.css for global styles and font settings.
    - Added favicon.ico for the application icon.

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import logo from '../logo.svg';
+import '../css/App.css';
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Movie.ca!
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a simple React application to showcase movie listings.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ratione quae eos enim ut porro, nobis, sequi culpa aliquid commodi dolor voluptatem? Consectetur, deserunt odit aliquam cum porro quasi ad?
+        </p>
+           
+      </header>
+      <main>
+        <h1>Featured Movies</h1>
+        <ul>
+          <li>Movie 1</li>
+          <li>Movie 2</li>
+          <li>Movie 3</li>
+        </ul>
+        <p>
+          Check out our latest releases and enjoy your movie experience!
+        </p>
+      </main>
+      <footer>
+        <p>&copy; 2023 Movie.ca</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
