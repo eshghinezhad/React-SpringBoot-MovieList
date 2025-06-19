86b5bebfda8a91a6a4508889243f4bf73f9c82ad
Monire En <monire.eshghi@gmail.com>
2025-06-18 23:40:30 -0400

    feat: Add routing with react-router-dom and create About component

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header';
+import Search from './Search';
+import Form from './Form';
+import Footer from './Footer';
+
+function About() {
+  return (
+    <div>
+      <Header />
+      <Search />
+      <Form />
+      <Footer/>
+    </div>
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,30 @@ import Calculator from './Calculator';
 import CalcButton from './CalcButton';
 import Practice from   './Practice';
 import Footer from './Footer';
+import About from './About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 const App = () => {
   return (
     <div className="App">
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<Header />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/search" element={<Search />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/calcbutton" element={<CalcButton />} />
+          <Route path="/practice" element={<Practice />} />
+          <Route path="/footer" element={<Footer />} />
+        </Routes>
+      </Router>
+
       <Header />
-      <Search />
-      <Form />
       <Counter/>
       <Calculator />
       <CalcButton />
