0eb08458e4a1e519af07cc34cb7dd08a038969f4
Monire En <monire.eshghi@gmail.com>
2025-06-26 18:28:06 -0400

    feat: Update movie data in db.json and implement FeaturedMovies component with API fetching and Swiper integration

--- src/components/FeaturedMovies.js
+++ src/components/FeaturedMovies.js
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, {useEffect,useState} from "react";
+import { Box } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-function FeaturedMovies() {
+
+
+const FeaturedMovies = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+      fetch("http://localhost:3001/movies_Tvs")
+        .then(res => res.json())
+        .then(data => setMovies(data))
+        .catch(error => console.error("Error fetching movies:", error));
+    }, []);
+
   return (
-    <div style={{
-
-    }}> 
-      <h2>Featured Movies</h2>
-
-    </div>
-  )
-}
+    <Box 
+      sx={{
+        maxWidth: "900px",  
+        margin: "auto",  
+        mt: 1,  
+        borderRadius: "12px", 
+        overflow: "hidden",
+        boxShadow: 3
+      }}
+    >
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={10}
+        slidesPerView={1}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        navigation
+        pagination={{ clickable: true }}
+        loop
+      >
+        {movies.map((movie) => (
+          <SwiperSlide key={movie.id}>
+            <Box
+              component="img"
+              src={movie.poster}
+              alt={movie.title}
+              sx={{
+                width: "100%",
+                height: "400px",
+                objectFit: "cover" 
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+};
 
 export default FeaturedMovies
