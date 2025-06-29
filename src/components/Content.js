f757cc7e4379057fa9ae8460e5d85111bda35ac8
Monire En <monire.eshghi@gmail.com>
2025-06-28 23:15:22 -0400

    feat: Remove unused imports in Content component; add Vercel configuration for deployment

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Typography, Grid, Box } from "@mui/material";
+import { Card, CardMedia, Typography, Grid, Box } from "@mui/material";
 
 const Content = () => {
   const contentData = [
