c61eccd66f8167209519bc002a490d8eb22b3ad2
Monire En <monire.eshghi@gmail.com>
2025-07-01 01:39:12 -0400

    Updated Server.js to fide the error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const path = require("path");
-// const jsonServer = require("json-server");
+const jsonServer = require("json-server");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
